1fdeac1f2f6278980c59d6a143c81ccfbf279d4c
Cadmium <512256738@qq.com>
2018-09-14 14:58:11 -0500

    try

---
+++ float2fix.m
@@ -0,0 +1,24 @@
+scale = 2^9;
+A_fix = zeros(1,256);
+B_fix = zeros(256,676);
+C_fix = zeros(1,676);
+
+
+% for i = 1:256
+%     A_fix(i) = round(A_fix)*scale;
+% end
+
+% for i = 1:256
+%     for j = 1:676
+%     B_fix(i,j) = round(B_fix)*scale;
+%     end
+% end
+
+A_fix = round(A*scale);
+B_fix = round(B*scale);
+
+% C = A_fix * B_fix/scale;
+C_fix = A_fix * B_fix/scale/scale;
+C_flp = A * B;
+ddif = C_fix - C_flp;
+disp(['SNR is', num2str(10*log10(sum(C_flp(:).^2)/sum(ddif(:).^2))), 'dB']);
