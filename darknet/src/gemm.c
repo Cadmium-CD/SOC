11157a3e33f46ef5db6fc63bd27a9b92946890a1
Jestice08 <34907435+Jestice08@users.noreply.github.com>
2018-09-22 15:25:30 -0500

    Add files via upload
    float2fix(int) B //no round
    round(A_1) 
    1.6sec 79% zy

--- darknet/src/gemm.c
+++ darknet/src/gemm.c
@@ -25,7 +25,7 @@ int roundup(float fp_number)
 int float2fix(float FloatPointValue)
 {
     int scale = 8;
-    int result = (int)round(FloatPointValue*(1<<scale));
+    int result = (int)(FloatPointValue*(1<<scale));
     return result;
 
 
@@ -1617,21 +1617,19 @@ void gemm_nn(int M, int N, int K, float ALPHA,
     float *C, int ldc)
 {
 		int ALPHA_1 = float2fix(ALPHA);
-
-
     int i, j, k, q, p;
     int A_1[M*K];
 		for(q=0;q<M*K;q++)
 		{
 			A_1[q] = (int)round(A[q] * (1 << 8));
 		}
-
-	for (p = 0; p < N*K;p++)
+/*	for (p = 0; p < N*K;p++)
 	{
     //if(p<1010 & p>1000) printf("float = %f  ", B[p]);
-		B[p] =(int) (B[p] * (1 << 8));
-    //if(p<1010 & p>1000) printf("fix = %d\n", B[p]);
+		B[p] =(int)( (B[p] * (1 << 8)));
+    //if(p<1010 & p>1000) printf("fix = %f\n", B[p]);
 	}
+*/
     //int B_1[N*K];
     // for(q = 0; q < K*N; q++)
     // {
@@ -1642,19 +1640,21 @@ void gemm_nn(int M, int N, int K, float ALPHA,
 		    //register float A_PART_1 = fix2float(ALPHA_1*float2fix(A[i*lda + k]),2);
     		  register float A_PART_1 = (float)(ALPHA_1*A_1[i*lda + k])/(1 << 16);
             for (j = 0; j < N; ++j) {
-				C[i*ldc + j] += (float)(float2fix(A_PART_1) * B[k*ldb + j])/(1<<16);
+								C[i*ldc + j] += (float)(float2fix(A_PART_1) * float2fix(B[k*ldb + j]))/(1<<16);
 								//C[i*ldc + j] = fix2float(C[i*ldc + j]);
                 /*C[i*ldc + j] += fix2float(A_PART*float2fix(B[k*ldb + j]));
 								B[K*ldb + j] = fix2float(B[k*ldb + j]);*/
             }
         }
     }
-		for (p = 0; p < N*K;p++)
+/*		for (p = 0; p < N*K;p++)
 		{
 			//if(p<1010 & p>1000) printf("float = %f  ", B[p]);
+
 			B[p] =((float)B[p] / (1 << 8));
 			//if(p<1010 & p>1000) printf("fix = %d\n", B[p]);
 		}
+		*/
 }
 
 
