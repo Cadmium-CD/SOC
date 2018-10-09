8e58ee28db1dbcac777a8db29d864aac3b36ddf2
Cadmium-CD <512256738@qq.com>
2018-10-09 12:39:13 -0500

    Add files via upload
    un maked 
    acceleratordev

---
+++ systemctlm-cosim-demo/acceleratordev.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2013 Xilinx Inc.
+ * Written by Edgar E. Iglesias.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+class acceleratordev
+: public sc_core::sc_module
+{
+public:
+	tlm_utils::simple_target_socket<acceleratordev> mastersocket;
+	//sc_out<bool> irq;
+	typedef acceleratordev SC_CURRENT_USER_MODULE;
+		acceleratordev(sc_core::sc_module_name name);
+  void acc_thread();
+};
