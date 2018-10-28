1eaf0533297be7c59543ee3f1dcae63cebcc3ee5
Yi Zheng <zengngai@hust.edu.cn>
2018-10-28 14:40:17 -0500

    add cosimulation code

---
+++ cosimulation/systemctlm-cosim-demo/zynqmp_demo.cc
@@ -0,0 +1,68 @@
+/*
+ * Top level of the ZynqMP cosim example.
+ *
+ * Copyright (c) 2014 Xilinx Inc.
+ * Written by Edgar E. Iglesias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "systemc.h"
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+#include "tlm_utils/tlm_quantumkeeper.h"
+
+using namespace sc_core;
+using namespace sc_dt;
+using namespace std;
+
+#include "trace.h"
+#include "iconnect.h"
+#include "memory.h"
+
+#include "demo-dma.h"
+#include "xilinx-zynqmp.h"
+
+#include "tlm2apb-bridge.h"
+#ifdef HAVE_VERILOG_VCS
+#include "apb_slave_timer.h"
+#endif
+
+#ifdef HAVE_VERILOG_VERILATOR
+#include "Vapb_timer.h"
+#include "verilated.h"
+#endif
+
+#define NR_DEMODMA      4
+#define NR_MASTERS	1 + NR_DEMODMA
+#define NR_DEVICES	4 + NR_DEMODMA
+
+
+int sc_main(int argc, char* argv[])
+{
+
+	return 0;
+}
