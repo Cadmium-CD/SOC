efe1a68bf4bd3713b9d0c06404e048a697250f2a
stzut <43415050+stzut@users.noreply.github.com>
2018-10-28 14:53:33 -0500

    Add files via upload

---
+++ lab4/CMakeFiles/gemm_lab4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gemm_lab4.dir/src/gemm_lab4.c.o"
+  "CMakeFiles/gemm_lab4.dir/src/testbench.c.o"
+  "bin/gemm_lab4.pdb"
+  "bin/gemm_lab4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gemm_lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
