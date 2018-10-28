667016f763fd929744425096ca07a6254f653ff2
stzut <43415050+stzut@users.noreply.github.com>
2018-10-28 14:23:06 -0500

    Add files via upload

---
+++ lab4/gemm_lab4.h
@@ -0,0 +1,9 @@
+#ifndef GEMM_LAB4_H
+#define GEMM_LAB4_H
+#include <math.h>
+#include <stdint.h>
+#include <stddef.h>
+
+void gemm_lab4 (int lda, int ldb,int ldc,int M, int N, int K, int* A, int* B, int* C);
+
+#endif
